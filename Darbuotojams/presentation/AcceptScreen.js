87beb6957ac3f4945b2cd9d4d5b4a458bfc0d605
GabijaPosk <90755238+GabijaPosk@users.noreply.github.com>
2024-01-09 13:44:31 +0200

    Update AcceptScreen.js

--- Darbuotojams/presentation/AcceptScreen.js
+++ Darbuotojams/presentation/AcceptScreen.js
@@ -1,61 +1,79 @@
-import React from 'react';
-import { View, Text, StyleSheet, Alert, TouchableHighlight } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Alert, TouchableHighlight, ScrollView, Modal, TouchableOpacity } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { confirmOrderAction } from '../redux/OrderSlice';
 
 const AcceptScreen = ({ route, navigation }) => {
   const dispatch = useDispatch();
-  const { order } = route.params;
+  const { order, prekės } = route.params;
+  const [successModalVisible, setSuccessModalVisible] = useState(false);
+
+  console.log('AcceptScreen order:', order);
+  console.log('AcceptScreen prekės:', prekės);
 
   const handleConfirmOrder = () => {
-    Alert.alert(
-      'Patvirtinimas',
-      'Ar jūs tikrai norite patvirtinti šį užsakymą?',
-      [
-        {
-          text: 'Grįžti',
-          onPress: () => navigation.goBack(), // Pakeitimas čia
-          style: 'cancel', // Pakeitimas čia
-        },
-        {
-          text: 'Patvirtinti',
-          onPress: () => {
-            dispatch(confirmOrderAction(order.id));
-            navigation.navigate('Pagrindinis'); 
-          },
-        },
-      ],
-      { cancelable: false }
-    );
+    dispatch(confirmOrderAction(order.id));
+    setSuccessModalVisible(true);
   };
+
+  const handleModalClose = () => {
+    setSuccessModalVisible(false);
+    navigation.navigate('Pagrindinis');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Patvirtinti užsakymą</Text>
-      <View style={styles.infoBlock}>
-        <Text style={styles.infoLabel}>Pavadinimas:</Text>
-        <Text style={styles.infoText}>{order.pavadinimas}</Text>
-        <Text style={styles.infoLabel}>Kiekis:</Text>
-        <Text style={styles.infoText}>{order.kiekis}</Text>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.title}>Patvirtinti užsakymą</Text>
+        <View style={styles.infoBlock}>
+          {order.prekės && order.prekės.map((prekė, index) => (
+            <View key={index} style={styles.prekėContainer}>
+              <View style={styles.prekėInfoContainer}>
+                <Text>{`Pavadinimas: ${prekė.PAVADINIMAS}`}</Text>
+                <Text>{`Kiekis: ${prekė.kiekis}`}</Text>
+                <Text>{`Kaina: ${prekė.KAINA}`}</Text>
+                <Text>{`Viso Kaina: ${prekė.visoKaina}`}</Text>
+              </View>
+            </View>
+          ))}
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableHighlight
+            style={styles.button}
+            underlayColor="#FBCFCD"
+            onPress={handleConfirmOrder}
+          >
+            <Text style={styles.buttonText}>Patvirtinti užsakymą</Text>
+          </TouchableHighlight>
+          <View style={styles.buttonContainerback}>
+            <TouchableHighlight
+              style={styles.buttoncancel}
+              underlayColor="#FBCFCD"
+              onPress={() => navigation.goBack()}
+            >
+              <Text style={styles.buttonText}>Grįžti</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
       </View>
-      <View style={styles.buttonContainer}>
-        <TouchableHighlight
-          style={styles.button}
-          underlayColor="#FBCFCD"
-          onPress={handleConfirmOrder}
-        >
-          <Text style={styles.buttonText}>Patvirtinti užsakymą</Text>
-        </TouchableHighlight>
-        <View style={styles.buttonContainerback}>
-        <TouchableHighlight
-          style={styles.buttoncancel}
-          underlayColor="#FBCFCD"
-          onPress={() => navigation.goBack()}
-        >
-          <Text style={styles.buttonText}>Grįžti</Text>
-        </TouchableHighlight>
-      </View>
-      </View>
-    </View>
+      <Modal
+        transparent={true}
+        animationType="slide"
+        visible={successModalVisible}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalText}>Užsakymas sėkmingai patvirtintas!</Text>
+            <TouchableOpacity
+              style={styles.modalButton}
+              onPress={handleModalClose}
+            >
+              <Text style={styles.modalButtonText}>Uždaryti</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+    </ScrollView>
   );
 };
 const styles = StyleSheet.create({
@@ -112,6 +130,32 @@ const styles = StyleSheet.create({
   infoText: {
     color: '#997070',
     marginBottom: 20,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  modalButton: {
+    backgroundColor: '#709999',
+    padding: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  modalButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
 
