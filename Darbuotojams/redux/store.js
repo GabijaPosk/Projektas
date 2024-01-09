5c8d9d26bc72dda793b5661d0358a39893c314e0
GabijaPosk <90755238+GabijaPosk@users.noreply.github.com>
2024-01-09 13:47:05 +0200

    Update store.js

--- Darbuotojams/redux/store.js
+++ Darbuotojams/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import OrderReducer from './OrderSlice';
+import MenuReducer from './MenuSlice';
 
 const store = configureStore({
   reducer: {
     orders: OrderReducer,
+    menuItems: MenuReducer,
   },
 });
 
